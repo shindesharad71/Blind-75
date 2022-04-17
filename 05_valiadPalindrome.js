9b0f370887c2bbff393b8f4a85ab77fe21a36d6d
Sharad Shinde <sharad.shinde@kuvera.in>
2022-04-17 16:08:47 +0530

    Submission for Valid Palindrome Problem

---
+++ 05_valiadPalindrome.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = function (s) {
+  s = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
+  const reverseString = s.split("").reverse().join("");
+  return s === reverseString;
+};
+
+// const s = "A man, a plan, a canal: Panama";
+const s = "0P";
+console.log(isPalindrome(s));
+
+// Expected output -> true
