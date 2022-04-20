d32fb207ba1558c81443340ff1cad547d8ef6af0
Sharad Shinde <sharad.shinde@kuvera.in>
2022-04-20 21:49:57 +0530

    Submission for Lowest Common Ancestor Problem

---
+++ 11_lowestCommonAncestorBST.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+const lowestCommonAncestor = function (root, p, q) {
+  let current = root;
+
+  while (current) {
+    if (p.val > current.val && q.val > current.val) {
+      current = current.right;
+    } else if (p.val < current.val && q.val < current.val) {
+      current = current.left;
+    } else {
+      return current;
+    }
+  }
+};
